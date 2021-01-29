4cc2243a638a2cacb2f3f1438ff9f68b7fe070aa
Manoel Blue <manoelbluee@gmail.com>
2021-01-29 16:19:11 +0100

    handle errors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 app.set('view engine', 'pug');
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
+
+// Set routes:
 app.get('/', (req, res) => {
     res.render('index', {projects: data.projects});
 })
@@ -18,6 +20,33 @@ app.get('/about', (req, res) => {
 
 app.get('/projects/:id', (req, res) => {
     res.render('project');
+});
+
+// Intentional 500 error:
+app.use('/500', (req, res, next) => {
+    const err500 = new Error('Error 500');
+    err500.status = 500;
+
+    res.locals.error = err500;
+    res.status(err500.status);
+    res.render('error', err500);
+});
+
+// 404 hanlder:
+app.use((req, res, next) => {
+    const err = new Error('The page was NOT FOUND.');
+    err.status = 404;
+    next(err);
+})
+
+
+// Handle errors:
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    if (err.message && err.status) {
+        res.render('error', err);
+    }
 });
 
 app.listen(4000, () => {
